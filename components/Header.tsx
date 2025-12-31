dd4e08b5ae5541e57e3e2ca9977cc57074cafea9
Earl Jonas Tigno <earljonastigno@gmail.com>
2025-12-31 14:05:54 +0800

    navigation added

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { cn } from "@/lib/utils";
+
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+const Header = () => {
+    const pathname = usePathname();
+    return <header>
+        <div className="main-container inner">
+            <Link href="/">
+                <Image src="logo.svg" alt="logo" width={132} height={40} />
+            </Link>
+
+            <nav>
+                <Link href='/' className={cn('nav-link', {
+                    'is-active': pathname === '/',
+                    'is-home': true
+                })}>Home</Link>
+
+                <p>Search Modal</p>
+
+                <Link href="/coins" className={cn('nav-link', {
+                    'is-active': pathname === '/coins',
+                })}>All Coins</Link>
+            </nav>
+        </div>
+    </header>;
+};
+
+export default Header;
