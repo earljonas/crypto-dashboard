0b37dc498b96f18790e818f4d209e3e538c468cd
Earl Jonas Tigno <earljonastigno@gmail.com>
2026-01-01 21:19:43 +0800

    starting homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Page = () => {
-  return <div></div>
+  return <main className="main-container">
+    <section className="home-grid">
+      <p>Coin Overview</p>
+
+      <p>Trending Coins</p>
+    </section>
+
+    <section className="w-full mt-7 space-y-4">
+      <p>Categories</p>
+    </section>
+
+  </main>
 };
 
 export default Page;
