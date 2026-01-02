604d8e6f8c70b8075352ded039739a1b616a590c
Earl Jonas Tigno <earljonastigno@gmail.com>
2026-01-02 22:29:33 +0800

    continuing homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import Image from "next/image";
+import DataTable from "@/components/DataTable";
 
 const Page = () => {
   return <main className="main-container">
     <section className="home-grid">
-      <p>Coin Overview</p>
+      <div id="coin-overview"> 
+        <div className="header pt-2">
+          <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" 
+          alt="Bitcoin" width={56} height={56} />
+          <div className="info">
+            <p>"Bitcoin / BTC</p>
+            <h1>$89,113.00</h1>
+          </div>
+        </div>
+      </div>
 
       <p>Trending Coins</p>
+      <DataTable />
     </section>
 
     <section className="w-full mt-7 space-y-4">
